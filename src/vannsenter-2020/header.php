6dcc7f4f55dac151d859f72bd5f2d674578096ee
Eirik Refsdal <eirikref@gmail.com>
2020-10-16 12:54:18 +0200

    Header, footer, fonts and styles

--- src/vannsenter-2020/header.php
+++ src/vannsenter-2020/header.php
@@ -19,7 +19,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
-
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 		<?php wp_head(); ?>
 
 	</head>
